cc2f228bbfed6f2c4a43a49015077d3244102f95
Cl3r1k <Cl3r1k2@gmail.com>
2020-06-14 08:38:46 +0700

    feat: Add 'stylelint-order' feat: Update stylelint config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,4 +3,25 @@
 
 module.exports = {
   extends: 'stylelint-config-standard',
+  plugins: [
+    // stylelint plugin to sort CSS rules content with specified order
+    // https://github.com/hudochenkov/stylelint-order
+    'stylelint-order',
+  ],
+
+  rules: {
+    // Specify the order of content within declaration blocks
+    // https://github.com/hudochenkov/stylelint-order/blob/master/rules/order/README.md
+    'order/order': [
+      'custom-properties',
+      'dollar-variables',
+      'declarations',
+      'at-rules',
+      'rules',
+    ],
+
+    // Specify the order of properties within declaration blocks
+    // https://github.com/hudochenkov/stylelint-order/blob/master/rules/properties-order/README.md
+    'order/properties-order': ['width', 'height'],
+  },
 };
