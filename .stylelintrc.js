826c614d48f70160967dd2183c299c2e17db12d3
Cl3r1k <Cl3r1k2@gmail.com>
2020-06-15 08:32:50 +0700

    ✔ feat: Implement stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,7 +2,10 @@
 // https://stylelint.io/user-guide/configure
 
 module.exports = {
+  // The standard config based on a handful of CSS style guides
+  // https://github.com/stylelint/stylelint-config-standard
   extends: 'stylelint-config-standard',
+
   plugins: [
     // stylelint plugin to sort CSS rules content with specified order
     // https://github.com/hudochenkov/stylelint-order
@@ -10,6 +13,33 @@ module.exports = {
   ],
 
   rules: {
+    // Disallow unknown properties
+    // https://stylelint.io/user-guide/rules/property-no-unknown#property-no-unknown
+    'property-no-unknown': [
+      true,
+      {
+        ignoreProperties: [
+          // CSS Modules composition
+          // https://github.com/css-modules/css-modules#composition
+          'composes',
+        ],
+      },
+    ],
+
+    // Disallow unknown pseudo-class selectors
+    // https://stylelint.io/user-guide/rules/selector-pseudo-class-no-unknown#selector-pseudo-class-no-unknown
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: [
+          // CSS Modules :global scope
+          // https://github.com/css-modules/css-modules#exceptions
+          'global',
+          'local',
+        ],
+      },
+    ],
+
     // Specify the order of content within declaration blocks
     // https://github.com/hudochenkov/stylelint-order/blob/master/rules/order/README.md
     'order/order': [
