ce6cf716e9837a2ecb41058a585fe720298edcaf
Cl3r1k <Cl3r1k2@gmail.com>
2020-06-22 08:44:24 +0700

    feat: add @commitlint feat: update husky config

---
+++ .commitlintrc.js
@@ -0,0 +1,6 @@
+// commitlint checks if your commit messages meet the conventional commit format (https://conventionalcommits.org)
+// https://commitlint.js.org/#/guides-local-setup
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+};
