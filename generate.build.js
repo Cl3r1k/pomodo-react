81dff8a6b1b2b7db2bfb62d43c7a6b25d3dda9c4
Cl3r1k <Cl3r1k2@gmail.com>
2020-06-24 08:51:27 +0700

    feat: ✔ implement autoincrement build through 'generate.build.js'

--- generate.build.js
+++ generate.build.js
@@ -1,9 +1,29 @@
-// DESCRIPTION: Take current version from package.json, update build from metadata.json, combine and save it
+// DESCRIPTION: Take current version from package.json, update build from metadata.json, combine it and save
 
-// const fs = require('fs');
+const fs = require('fs');
 const packageFile = require('./package.json');
 
 const packageVersion = packageFile.version;
 
-console.info('Incrementing build number...');
-console.info('packageVersion: ', packageVersion);
+fs.readFile('src/metadata.json', (err, content) => {
+  if (err) throw err;
+
+  const metadata = JSON.parse(content);
+
+  metadata.build = `000${+metadata.build + 1}`.slice(-4);
+  metadata.version = `${packageVersion}.${metadata.build}`;
+
+  fs.writeFile('src/metadata.json', JSON.stringify(metadata), errWrite => {
+    if (errWrite) throw errWrite;
+
+    console.info(
+      '\x1b[36m',
+      'Build version set to: ',
+      '\x1b[32m',
+      metadata.version,
+      '\x1b[0m'
+    );
+  });
+});
+
+// Idea took from `https://github.com/facebook/create-react-app/issues/1917#issuecomment-291468057`
