a0a326bf76f3ccc50cd0881c29abc7a1b4016b50
Cl3r1k <Cl3r1k2@gmail.com>
2020-07-02 08:58:08 +0700

    feat: update husky config

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,13 +1,15 @@
 // Husky can prevent bad git commit, git push and more
 // https://github.com/typicode/husky
 // Supported git-hooks: https://git-scm.com/docs/githooks
+// Additional info https://www.codeflow.site/ru/article/how-to-use-git-hooks-to-automate-development-and-deployment-tasks
 
 module.exports = {
   hooks: {
     'pre-commit': 'lint-staged',
-    'commit-msg':
-      'node ./generate.build.js && git add ./src/metadata.json && commitlint -E HUSKY_GIT_PARAMS',
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
+    'applypatch-msg': 'node ./generate.build.js && git add ./src/metadata.json',
   },
 };
 
 // Consider to use 'applypatch-msg' or 'pre-applypatch' or 'post-applypatch' hooks for 'new build generation'
+// and try with 'prepare-commit-msg' hook
