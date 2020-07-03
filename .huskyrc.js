4717ff529065720e4d919c9822e7b3162247313a
Cl3r1k <Cl3r1k2@gmail.com>
2020-07-03 08:42:25 +0700

    feat: update husky config

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,7 +1,7 @@
 // Husky can prevent bad git commit, git push and more
 // https://github.com/typicode/husky
 // Supported git-hooks: https://git-scm.com/docs/githooks
-// Additional info https://www.codeflow.site/ru/article/how-to-use-git-hooks-to-automate-development-and-deployment-tasks
+// Additional info about hooks order https://www.codeflow.site/ru/article/how-to-use-git-hooks-to-automate-development-and-deployment-tasks
 
 module.exports = {
   hooks: {
@@ -11,6 +11,3 @@ module.exports = {
       'node ./generate.build.js && git add ./src/metadata.json',
   },
 };
-
-// Consider to use 'applypatch-msg' or 'pre-applypatch' or 'post-applypatch' hooks for 'new build generation'
-// and try with 'prepare-commit-msg' hook
