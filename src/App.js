6fea945d6ba2f6641d4520d7343f0aa774694f8a
Cl3r1k <Cl3r1k2@gmail.com>
2020-07-08 08:39:25 +0700

    feat: add 'AboutPage.js' with route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 // Pages
 import { HomePage } from 'pages/HomePage';
+import { AboutPage } from 'pages/AboutPage';
 import { NotFoundPage } from 'pages/NotFoundPage';
 
 // Styles
@@ -13,20 +14,28 @@ export const App = () => {
     <Router>
       <div className={styles.App}>
         <header className={styles['App-header']}>
-          <ul>
-            <li>
-              <Link to="/">Home Page</Link>
-            </li>
-            <li>
-              <Link to="/not-found">Not Found Page</Link>
-            </li>
-          </ul>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home Page</Link>
+              </li>
+              <li>
+                <Link to="/about">About Page</Link>
+              </li>
+              <li>
+                <Link to="/not-found">Not Found Page</Link>
+              </li>
+            </ul>
+          </nav>
         </header>
 
         {/* Routes */}
         <Switch>
           <Route exact path="/">
             <HomePage />
+          </Route>
+          <Route path="/about">
+            <AboutPage />
           </Route>
           <Route path="/*">
             <NotFoundPage />
--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 export const NotFoundPage = () => {
-  return <div>Not Found Page</div>;
+  const { pathname } = useLocation();
+  return <div>Not Found Page at: {pathname}</div>;
 };
