df46099e5dfd6617582722251065b262e5b2700d
Cl3r1k <Cl3r1k2@gmail.com>
2020-07-20 08:45:04 +0700

    feat: improve 'LoginPage'

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 
 // Hooks
@@ -14,22 +14,33 @@ export const LoginPage = () => {
   const history = useHistory();
   const location = useLocation();
   const authDispatch = useAuthDispatch();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
 
   const { from } = location.state || { from: { pathname: '/' } };
-  const handleOnSubmitSignIn = evt => {
+  const handleOnSubmitSignIn = async evt => {
     evt.preventDefault();
 
+    setLoading(true);
+    setError('');
+
     const formElements = evt.target.elements;
-    console.info('formElements: ', formElements);
+    // console.info('formElements: ', formElements);
     const userDetails = {
       email: formElements.email.value,
       password: formElements.password.value,
     };
-    console.info('userDetails: ', userDetails);
-    // authContext.authenticate(() => history.replace(from));
-    authActionSignIn(authDispatch, userDetails, () => history.replace(from));
-    // authDispatch({ type: 'SIGN_IN' });
-    // history.replace(from);
+    // console.info('userDetails: ', userDetails);
+
+    try {
+      await authActionSignIn(authDispatch, userDetails, () =>
+        history.replace(from)
+      );
+    } catch (err) {
+      setError(err);
+      console.info(error);
+      setLoading(false);
+    }
   };
 
   return (
@@ -39,15 +50,17 @@ export const LoginPage = () => {
       <h3>You must login to view the page at {from.pathname}</h3>
 
       <form onSubmit={handleOnSubmitSignIn}>
-        <label htmlFor="email">
-          <input type="text" name="email" id="email" />
-        </label>
+        <fieldset disabled={loading}>
+          <label htmlFor="email">
+            <input type="text" name="email" id="email" required />
+          </label>
 
-        <label htmlFor="password">
-          <input type="password" name="password" id="password" />
-        </label>
+          <label htmlFor="password">
+            <input type="password" name="password" id="password" required />
+          </label>
 
-        <button type="submit">Sign in</button>
+          <button type="submit">Sign in</button>
+        </fieldset>
       </form>
     </div>
   );
