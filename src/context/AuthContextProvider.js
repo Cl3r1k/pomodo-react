278abe8b2932a08bfe15cff3f69f2a5df4df1774
Cl3r1k <Cl3r1k2@gmail.com>
2020-07-23 08:44:43 +0700

    feat: improve 'AuthContextProvider' and 'authReducer'

--- src/context/AuthContextProvider.js
+++ src/context/AuthContextProvider.js
@@ -7,8 +7,17 @@ import { AuthStateContext, AuthDispatchContext } from 'context/AuthContext';
 // Reducers
 import { authReducer } from 'reducers/authReducer';
 
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+  clientId: process.env.REACT_APP_CLIENT_ID,
+  clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+  redirectUri: process.env.REACT_APP_REDIRECT_URI,
+  proxyUrl: process.env.REACT_APP_PROXY_URL,
+};
+
 export const AuthContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReducer, { isAuthenticated: false });
+  const [state, dispatch] = useReducer(authReducer, initialState);
 
   return (
     <AuthStateContext.Provider value={state}>
