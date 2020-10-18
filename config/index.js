b8684f23e6f45ce56d8c5397747c27132cc11288
Cl3r1k <cl3r1k2@gmail.com>
2020-10-18 11:56:47 +0700

    refactor: update app 'config'

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   someConfigProperty: 'Some Config Property',
   clientIdGithub: process.env.REACT_APP_CLIENT_ID,
-  proxyUrl: process.env.REACT_APP_PROXY_URL,
   clientSecretGithub: process.env.REACT_APP_CLIENT_SECRET,
+  proxyUrl: process.env.REACT_APP_PROXY_URL,
+  redirectUri: process.env.REACT_APP_REDIRECT_URI,
 };
