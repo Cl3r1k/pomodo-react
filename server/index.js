d3b10edb57b9e366fc69614917d0d9d280b2949e
Cl3r1k <cl3r1k2@gmail.com>
2020-10-26 08:50:13 +0700

    refactor: update 'proxy-server'

--- server/index.js
+++ server/index.js
@@ -70,5 +70,5 @@ app.post('/authenticate', (req, res) => {
     });
 });
 
-const PORT = process.env.SERVER_PORT || 5000;
+const PORT = config.serverPort || 5000;
 app.listen(PORT, () => console.info(`Server listening on port: ${PORT}`));
