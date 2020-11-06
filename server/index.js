46dd671e30be7f581e3b01e3f92464739e349c05
Cl3r1k <cl3r1k2@gmail.com>
2020-11-06 08:51:22 +0700

    refactor: update 'proxy-server'

--- server/index.js
+++ server/index.js
@@ -74,7 +74,7 @@ app.get('/', (req, res) => {
   res.status(200).json(`
     Avaialable server routes:
     GET '/':                  list of routes (API) (this page)
-    GET '/auth':              route for test config access
+    GET '/auth':              route for test config access (from .env file)
     POST '/authenticate':     proxy route to use authenticate API from other providers
   `);
 });
