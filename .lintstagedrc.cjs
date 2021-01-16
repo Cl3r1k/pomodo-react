e77f2e9f5b2fb25046e25d929ec40de5ff7436b9
Cl3r1k <cl3r1k2@gmail.com>
2021-01-16 09:05:57 +0700

    refactor: update 'lint-staged' configs

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -2,7 +2,7 @@
 // https://github.com/okonet/lint-staged
 
 module.exports = {
-  '*.+(js|jsx)': ['eslint', 'prettier --write'],
+  '*.+(js|jsx|ts|tsx)': ['eslint', 'prettier --write'],
   '*.+(json|md|graphql)': ['prettier --write'],
-  '*.+(css|less|styl|scss|sass|sss)': ['stylelint --fix'],
+  '*.+(css|less|styl|scss|sass|sss)': ['stylelint --fix']
 };
