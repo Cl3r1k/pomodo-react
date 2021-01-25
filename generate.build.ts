ea8d0b391e212b4f54713cf9bbfdca5abaa001a6
Cl3r1k <cl3r1k2@gmail.com>
2021-01-25 08:40:58 +0700

    feat: added types for 'generate.build.ts'

--- generate.build.ts
+++ generate.build.ts
@@ -1,6 +1,4 @@
 // TODO: Type all disabled errors
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 // DESCRIPTION: Take current version from package.json, update build from metadata.json, combine it and save
@@ -8,13 +6,18 @@
 import { readFile, writeFile } from 'fs';
 import packageFile from './package.json';
 
+type TMetadata = {
+  build: string;
+  version: string;
+};
+
 const packageVersion = packageFile.version;
 
 readFile('metadata.json', (err, content) => {
   if (err) throw err;
 
   // @ts-ignore
-  const metadata = JSON.parse(content);
+  const metadata: TMetadata = JSON.parse(content);
 
   metadata.build = `000${+metadata.build + 1}`.slice(-4);
   metadata.version = `${packageVersion}.${metadata.build}`;
