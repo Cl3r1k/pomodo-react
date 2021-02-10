257e5e4e3f2d0f36ae812703afe1dbe9bf22b558
Cl3r1k <cl3r1k2@gmail.com>
2021-02-10 09:58:00 +0700

    refactor: update '@utils/common'

--- utils/common.ts
+++ utils/common.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 type ParseResult<T> =
   | { parsed: T; hasError: false; error?: undefined }
   | { parsed?: undefined; hasError: true; error?: unknown };
@@ -18,26 +16,27 @@ export const safeJsonParse = <T>(typeGuard: (o: unknown) => o is T) => (
 export const delay = (ms: number): Promise<void> =>
   new Promise(res => setTimeout(() => res(), ms));
 
-export const delayCallback = (cb: (...args: unknown[]) => void, ms: number) =>
-  setTimeout(cb, ms);
+export const delayCallback = (
+  cb: (...args: unknown[]) => void,
+  ms: number
+): NodeJS.Timeout => setTimeout(cb, ms);
 
-// export const combineToQuery = <T extends Record<string, string | number>>(
-//   params: T,
-//   delimiter = '&'
-// ): string => {
-//   const keys: string[] = Object.keys(params);
-//   // const keys = Object.keys<string>(params);
+export const combineToQuery = <T extends Record<string, string | number>>(
+  params: T,
+  delimiter = '&'
+): string => {
+  const keys = Object.keys(params);
 
-//   return keys.reduce((acc, key, idx) => {
-//     const param = `${acc}${key}=${params[key]}`;
+  return keys.reduce((acc, key, idx) => {
+    const param = `${acc}${key}=${params[key]}`;
 
-//     if (idx < keys.length - 1) return param + delimiter;
+    if (idx < keys.length - 1) return param + delimiter;
 
-//     return param;
-//   }, '');
-// };
+    return param;
+  }, '');
+};
 
-export const combineToParams = (query: string) => {
+export const combineToParams = (query: string): Record<string, string> => {
   const q = query.replace(/^\??\//, '');
 
   return q.split('&').reduce((values, param) => {
